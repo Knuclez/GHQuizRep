fa3af774e1dbbd1e6307098e7e5aa65f0ae7a95e
Knuclez <MDiazMoraiz@example.com>
2020-12-17 19:27:35 -0300

    ediciones basicas de archivos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Japanese_Hiragana_kyokashotai_A.svg/274px-Japanese_Hiragana_kyokashotai_A.svg.png" alt="hiragana"/>
       </header>
     </div>
   );
